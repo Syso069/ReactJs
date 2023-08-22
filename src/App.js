e7e8c6cbeb8e449f9c98a0be1e2fce80af72b0f9
Syso <sisomd69@gmail.com>
2023-08-22 11:17:16 -0300

    reactJs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Banner from './componentes/Banner/Banner.js';
+import Formulario from './componentes/Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <Formulario/>
+    </div>
+  );
+}
+
+export default App;
