15381e5e72fcec7ab0a35cdada47fad4fbb2ba0e
Syso <sisomd69@gmail.com>
2023-08-23 10:33:57 -0300

    finalizando form

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,13 +1,35 @@
+/* eslint-disable no-undef */
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css"
 
 const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        ' Inovação e Gestão'
+        ]
+
+        const aoSalvar = () => {
+            evento.preventDefault();
+            console.log("Form foi submetido");
+        } 
+
     return (
         <section className="formulario">
-            <form>
-            <CampoTexto label="Nome" placeholder="Digite seu nome..."/>
-            <CampoTexto label="Cargo" placeholder="Digite seu cargo..."/> 
+            <form onSubmit={aoSalvar}>
+            <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome..."/>
+            <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo..."/> 
             <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem..."/> 
+            <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+            <Botao>
+                Criar Texto
+            </Botao>
             </form>
         </section>
     );
